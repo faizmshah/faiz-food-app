4dd075875f4c02c0c38566df37aa0e068c4821df
Faiz Muhammad Shah <tofeequeshah456@gmail.com>
2024-12-05 02:46:19 +0500

    sixth

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './src/**/*.{js,ts,jsx,tsx}', // Include all JS, TS, JSX, and TSX files in the src folder
+    './pages/**/*.{js,ts,jsx,tsx}', // If you have a pages folder
+    './components/**/*.{js,ts,jsx,tsx}', // If you have a components folder
+  ],
   theme: {
-    extend: {},
+    extend: {
+      container: { center: true,
+        padding: "15px",
+      },
+      colors:{
+accent:"#f94c25"
+      }
+      
+    },
   },
   plugins: [],
-}
-
+};
